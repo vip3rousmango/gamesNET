9255e97aa8689a492812f297e1275915d624c2a0
Al Romano <alexander@virtuallycreative.ca>
2019-08-31 19:37:16 -0400

    app init

---
+++ src/js/main.js
@@ -0,0 +1,4 @@
+import 'bootstrap'
+import '../../node_modules/bootstrap/dist/css/bootstrap.css' // Import precompiled Bootstrap css
+import '../../node_modules/@fortawesome/fontawesome-free/css/all.css'
+import '../css/styles.sass' // Import our scss file
